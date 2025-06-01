f3aa4bbf88e380ddc8b3d45a4e3f947c21cb981d
dean <alanxv1024@gmail.com>
2025-06-01 18:16:51 +0800

    test

--- providers/vertexai/base.go
+++ providers/vertexai/base.go
@@ -109,7 +109,7 @@ func (p *VertexAIProvider) GetToken() (string, error) {
 		return "", fmt.Errorf("failed to unmarshal credentials: %w", err)
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
 	defer cancel()
 
 	proxyAddr := ""
